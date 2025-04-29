c50cdc8421f79702fbbd8144d511ca02980973d5
chathubandaranayake <136330847+chathubandaranayake@users.noreply.github.com>
2025-04-29 16:19:25 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".textc", {
+    strings: ["an Undergraduate" , "a Web Developer" , "a Mobile Application Developer" , "a Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true,
+});
